c1ad524c328fe54781fdaba6e04b10033f2fe826
Riski Nofriadi <riskinofriadi049@gmail.com>
2023-12-22 14:54:33 +0700

    Final Project

---
+++ database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('mahasiswas')->insert([
+            'npm' => "101010",
+            'nama' => 'Mahasiswa10',
+            'alamat' => 'Padang',
+            'no_hp' => '08122222',
+        ]);
+    }
+}
